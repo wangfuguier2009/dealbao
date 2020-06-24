57ddd535ba18b6b8c42aefaa70404696e9ea85c6
cn-hideyoshi <645165778@qq.com>
2020-06-24 09:21:15 +0800

    update  && reformat code

--- src/Open/lib/goods/Goods.php
+++ src/Open/lib/goods/Goods.php
@@ -1,54 +1,79 @@
 <?php
+
 namespace Dealbao\Open\lib\goods;
+
 use Dealbao\Open\core\Api;
+
 class Goods extends Api
 {
     /**get simple goods list by cate
      * @param array $params
      * @return bool|string
      */
-    public function getGoodsListByCate(array $params = []){
-        return $res = $this->request('Goods/getGoodsListByCate',$params,'GET');
+    public function getGoodsListByCate(array $params = [])
+    {
+        return $res = $this->request('Goods/getGoodsListByCate', $params, 'GET');
     }
+
     /**search goods list
      * @param array $params
      */
-    public function searchGoods(array $params){
-        return $res = $this->request('Goods/searchGoods',$params,'GET');
+    public function searchGoods(array $params)
+    {
+        return $res = $this->request('Goods/searchGoods', $params, 'GET');
     }
+
+    /**
+     * search goods list
+     * @param array $params
+     * @return bool|string
+     */
+    public function getGoodsListByCateGroup(array $params)
+    {
+        return $res = $this->request('Goods/getGoodsListByCateGroup', $params, 'GET');
+    }
+
     /**get sku stock
      * @param array $params
      * @return bool|string
      */
-    public function getGoodsSkuStock(array $params = []){
-        return $res = $this->request('Goods/getGoodsSkuStock',$params,'GET');
+    public function getGoodsSkuStock(array $params = [])
+    {
+        return $res = $this->request('Goods/getGoodsSkuStock', $params, 'GET');
     }
+
     /**batch get sku stock
      * @param array $params
      * @return bool|string
      */
-    public function batchGetGoodsSKuStock(array $params = []){
-        return $res = $this->request('Goods/batchGetGoodsSKuStock',$params,'GET');
+    public function batchGetGoodsSKuStock(array $params = [])
+    {
+        return $res = $this->request('Goods/batchGetGoodsSKuStock', $params, 'GET');
     }
+
     /**check sku stock
      * @param array $params
      * @return bool|string
      */
-    public function batchCheckStock(array $params = []){
-        return $res = $this->request('Goods/batchCheckStock',$params,'GET');
+    public function batchCheckStock(array $params = [])
+    {
+        return $res = $this->request('Goods/batchCheckStock', $params, 'GET');
     }
 
     /**search spu stock
      * @param array $params
      */
-    public function getGoodsSpuStock(array $params){
-        return $res = $this->request('Goods/getGoodsSpuStock',$params,'GET');
+    public function getGoodsSpuStock(array $params)
+    {
+        return $res = $this->request('Goods/getGoodsSpuStock', $params, 'GET');
     }
+
     /**
      * get goods detail by sku
      */
-    public function getGoodsBySku(array $params){
-        return $res = $this->request('Goods/getGoodsBySku',$params,'GET');
+    public function getGoodsBySku(array $params)
+    {
+        return $res = $this->request('Goods/getGoodsBySku', $params, 'GET');
     }
 
     /**
@@ -62,7 +87,8 @@ class Goods extends Api
     /**get goods detal by sku_language
      * @param array $params
      */
-    public function getGoodsBySkuLanguage(array $params){
-        return $res = $this->request('Goods/getGoodsBySkuLanguage',$params,'GET');
+    public function getGoodsBySkuLanguage(array $params)
+    {
+        return $res = $this->request('Goods/getGoodsBySkuLanguage', $params, 'GET');
     }
 }
