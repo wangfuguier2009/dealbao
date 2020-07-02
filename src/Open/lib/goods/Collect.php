f7fcd1b7e5400c20335136607505b919a4a2a973
cn-hideyoshi <645165778@qq.com>
2020-07-02 11:31:13 +0800

    add api -> deleteCollectGoodsBySpu & add collect demo

--- src/Open/lib/goods/Collect.php
+++ src/Open/lib/goods/Collect.php
@@ -6,7 +6,8 @@ use Dealbao\Open\core\Api;
 
 class Collect extends Api
 {
-    /**get collected group list
+    /**
+     * get collected group list
      * @param array $params
      * @return bool|string
      */
@@ -24,5 +25,16 @@ class Collect extends Api
     {
         return $res = $this->request('Collect/getCollectGoodsByGroup', $param, 'POST');
     }
+
+    /**
+     * delete goods by spu
+     * @param array $param
+     * @return bool|string
+     */
+    public function deleteCollectGoodsBySpu(array $param)
+    {
+        return $res = $this->request('Collect/deleteCollectGoodsBySpu', $param, 'DELETE');
+    }
+
 
 }
