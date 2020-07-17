adb0dee9ee8d171e553ce1d403c5f438ef82e169
cn-hideyoshi <54949420+cn-hideyoshi@users.noreply.github.com>
2020-07-17 10:29:58 +0800

    add api

--- src/Open/lib/order/Order.php
+++ src/Open/lib/order/Order.php
@@ -1,20 +1,34 @@
 <?php
+
 namespace Dealbao\Open\lib\order;
+
 use Dealbao\Open\core\Api;
+
 class Order extends Api
 {
     /**get order list
      * @param array $params
      * @return bool|string
      */
-    public function getOrderList(array $params){
-        return $res = $this->request('Order/getOrderList',$params,'GET');
+    public function getOrderList(array $params)
+    {
+        return $res = $this->request('Order/getOrderList', $params, 'GET');
     }
+
     /**
      * create a new order method
      * @param array $params
      */
-    public function createAppOrder(array $params){
-        return $this->request('Order/createAppOrder',$params,'PUT',true);
+    public function createAppOrder(array $params)
+    {
+        return $this->request('Order/createAppOrder', $params, 'PUT', true);
+    }
+
+    /**
+     * get order info
+     */
+    public function getOrderInfo(array $params)
+    {
+        return $this->request('Order/getOrderInfo', $params, 'GET', true);
     }
 }
